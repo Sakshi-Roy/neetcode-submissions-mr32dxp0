0eeee8a992a9dc65cc47bc5628cdb3206042c4ca
Sakshi-Roy <70461164+Sakshi-Roy@users.noreply.github.com>
2026-04-08 09:59:18 +0530

    Add: count-servers-that-communicate - submission-1

---
+++ Data Structures & Algorithms/count-servers-that-communicate/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+int countServers(vector<vector<int>>& grid) 
+{
+    int m = grid.size();
+    int n = grid[0].size();
+
+    vector<int> row(m, 0), col(n, 0);
+
+    // Step 1: count
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(grid[i][j] == 1)
+            {
+                row[i]++;
+                col[j]++;
+            }
+        }
+    }
+
+    // Step 2: result
+    int count = 0;
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(grid[i][j] == 1 && (row[i] > 1 || col[j] > 1))
+            {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+};
